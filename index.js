93d3cb16c9acef8f19816738349a0c5c0bf05cc8
toyman640 <falakoadebayor@gmail.com>
2023-05-08 12:40:00 +0100

    Add luxon date to project

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import * as luxon from './node_modules/luxon/build/es6/luxon.js';
 import Library from './modules/manage-books.js';
 import showBooks from './modules/show-books.js';
 import showForm from './modules/new-book.js';
@@ -26,3 +27,9 @@ const contactButton = document.querySelector('#contact');
 booksButton.addEventListener('click', showBooks);
 formButton.addEventListener('click', showForm);
 contactButton.addEventListener('click', showContact);
+
+const dateSection = document.querySelector('.date');
+
+const time = luxon.DateTime.local().toLocaleString(luxon.DateTime.DATETIME_MED);
+
+dateSection.innerHTML = time;
